4135bffddc94c9edc69db377c02cb357bc64b075
Will Schaller <willschaller@gmail.com>
2018-07-02 18:06:03 +0200

    Convert ES5 classes to ES6

--- scripts/game.js
+++ scripts/game.js
@@ -1,269 +1,263 @@
-"use strict";
+class Game {
+  constructor() {
+    this.sentences = [];
+    this.currentSentenceNum = 0;
+    this.currentSentence;
+    this.gameLength = 10;
+    this.allowedAttempts = 3;
+    this.timed = false;
+    this.timer = 30;
+    this.timerMax = 30;
+    this.timerID;
+    this.zombieIntervalID;
+    this.zombieSpawnIntervalID;
+    this.zombieMode = false;
+  }
 
-function Game() {
-  this.sentences = [];
-  this.currentSentenceNum = 0;
-  this.currentSentence;
-  this.gameLength = 10;
-  this.allowedAttempts = 3;
-  this.timed = false;
-  this.timer = 30;
-  this.timerMax = 30;
-  this.timerID;
-  this.zombieIntervalID;
-  this.zombieSpawnIntervalID;
-  this.zombieMode = false;
-}
+  createPlayer(name) {
+    this.player = new Player();
+    this.player.setName(name);
+  }
 
-Game.prototype.createPlayer = function(name) {
-  this.player = new Player();
-  this.player.setName(name);
-};
+  getSentences(num, lang) {
+    this.sentences = getRandomSentences(num, lang);
+  }
 
-Game.prototype.getSentences = function(num, lang) {
-  this.sentences = getRandomSentences(num, lang);
-};
+  getProgressPercent(current, total) {
+    return Math.floor((current / total) * 100);
+  }
 
-Game.prototype.getProgressPercent = function(current, total) {
-  return Math.floor((current / total) * 100);
-};
-
-Game.prototype.startGame = function(option, lang) {
-  var self = this;
-  this.getSentences(this.gameLength, lang);
-  // If timed mode is on, start timer interval and show timer
-  if (option === "timed") {
-    this.timed = true;
-    showTimer();
-    drawProgressBar(self.getProgressPercent(self.timer, self.timerMax));
-    this.timerID = window.setInterval(function() {
-      drawTimer();
+  startGame(option, lang) {
+    const self = this;
+    this.getSentences(this.gameLength, lang);
+    // If timed mode is on, start timer interval and show timer
+    if (option === "timed") {
+      this.timed = true;
+      showTimer();
       drawProgressBar(self.getProgressPercent(self.timer, self.timerMax));
-      self.timerDecrement();
+      this.timerID = window.setInterval(() => {
+        drawTimer();
+        drawProgressBar(self.getProgressPercent(self.timer, self.timerMax));
+        self.timerDecrement();
+      }, 1000);
+    }
+    this.nextQuestion();
+    // Zombie Mode easter egg
+    const robotHead = document.getElementById("robot-head");
+    robotHead.addEventListener("dblclick", () => {
+      self.startZombieMode();
+      drawAttemptString("Welcome to THE PRONUNCIO OF THE DEAD!");
+    });
+  }
+
+  timerDecrement() {
+    this.timer--;
+    if (this.timer < 0) {
+      this.gameEnded();
+      window.clearInterval(this.timerID);
+      return;
+    }
+  }
+
+  handleContinueButton() {
+    hideContinueButton();
+    showNextButtons();
+    // disableSpeakButton();
+    if (!this.checkIfVoiceAvailable(this.currentSentence.bcp47)) {
+      disableRevealPronunciationButton();
+    }
+  }
+
+  handlePlayAgainButton() {
+    buildSplash();
+  }
+
+  handleRevealPronunciationButton() {
+    this.speakSentence(this.currentSentence.sentence, this.currentSentence.bcp47);
+  }
+
+  handleNextButton() {
+    this.currentSentenceNum++;
+    this.nextQuestion();
+    hideNextButtons();
+  }
+
+  nextQuestion() {
+    this.player.collectScore();
+    this.currentSentence = this.sentences[this.currentSentenceNum];
+    if (this.currentSentenceNum < this.gameLength) {
+      fillInData(
+        this.player.name,
+        this.player.score,
+        this.currentSentence.sentence,
+        this.currentSentence.language,
+        this.allowedAttempts
+      );
+      drawFlag(this.currentSentence.bcp47);
+      if (!this.timed) {
+        drawProgressBar(
+          this.getProgressPercent(this.currentSentenceNum, this.gameLength)
+        );
+      }
+      enableSpeakButton();
+      hideContinueButton();
+      hideSpeechBubble();
+      this.player.handleSpeakButtonClick();
+    } else {
+      this.gameEnded();
+    }
+  }
+
+  gameEnded() {
+    this.addScoreToHighScores();
+    buildGameOver(this.player.score, this.getHighScores(10));
+    if (this.zombieMode) {
+      document.body.classList.remove("bg-secondary");
+    }
+  }
+
+  addScoreToHighScores() {
+    const scoreObject = { playerName: this.player.name, score: this.player.score };
+    highScores.push(scoreObject);
+  }
+
+  cleanString(string) {
+    // Strip out punctuation with Regex and trim whitespace.
+    const regex = new RegExp(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+"'\\]/g);
+    return string
+      .replace(regex, "")
+      .trim()
+      .toLowerCase();
+  }
+
+  diffStrings(string1, string2) {
+    if (!(string1 && string2)) {
+      return null;
+    }
+    const cleanString1 = this.cleanString(string1);
+    const cleanString2 = this.cleanString(string2);
+
+    const diff = JsDiff.diffWordsWithSpace(cleanString1, cleanString2);
+    return diff;
+  }
+
+  calculateDiffScore(string1, string2) {
+    if (!(string1 && string2)) {
+      return null;
+    }
+
+    const cleanString1 = this.cleanString(string1);
+    const cleanString2 = this.cleanString(string2);
+    const similarity = dice.coefficient(cleanString1, cleanString2);
+    return Math.floor(similarity * 100);
+  }
+
+  speakSentence(sentence, languageBcp47String) {
+    const utterance = new SpeechSynthesisUtterance(sentence);
+    utterance.lang = languageBcp47String;
+    window.speechSynthesis.speak(utterance);
+  }
+
+  getAvailableSpeechSynthesisLanguages() {
+    const supported = [];
+    const voices = speechSynthesis.getVoices();
+    voices.forEach(({lang}) => {
+      supported.push(lang.slice(0, 2));
+    });
+    return supported;
+  }
+
+  checkIfVoiceAvailable(languageBcp47String) {
+    const supported = this.getAvailableSpeechSynthesisLanguages();
+    if (supported.includes(languageBcp47String)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  translateString(string, lang) {
+    // Note: This is a temporary method using a disposable free API key. Will eventually be replaced when the app has a backend.
+    const stringURI = encodeURI(string);
+    fetch(
+      `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180627T143038Z.1e878579d0dfe5ce.cace0e83481e4da16a038b62e31825c4764bb8c5&text=${stringURI}&lang=${lang}-en`,
+      {
+        method: "get"
+      }
+    )
+      .then(response => response.json())
+      .then(({text}) => drawTranslation(text[0]))
+      .catch(err => {
+        //error block
+      });
+  }
+
+  getHighScores(num) {
+    const scores = highScores.slice(0);
+
+    scores.sort(({score}, {score}) => score - score);
+
+    return scores.slice(0, num);
+  }
+
+  startZombieMode() {
+    const self = this;
+    this.zombieMode = true;
+    this.zombieSpawnIntervalID = window.setInterval(() => {
+      self.spawnZombie();
+    }, 4000);
+    game.startZombieChecking();
+    hideInputCard();
+    document.body.classList.add("bg-secondary");
+  }
+
+  stopZombieMode(zombieSpawnIntervalID) {
+    window.clearInterval(zombieSpawnIntervalID);
+  }
+
+  spawnZombie() {
+    const zombieSpawner = document.getElementById("zombie-spawner");
+    const zombie = document.createElement("div");
+    zombie.classList.add("zombie");
+    zombie.setAttribute(
+      "style",
+      `left: ${Math.random() * (window.innerWidth - 100)}px`
+    );
+    zombieSpawner.appendChild(zombie);
+  }
+
+  checkIfZombieWalkFinished() {
+    const self = this;
+    const zombies = document.querySelectorAll(".zombie:not(.zombie-dead)");
+    const viewportHeight = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight
+    );
+    zombies.forEach(({offsetTop}) => {
+      if (offsetTop > viewportHeight - 80) {
+        self.stopZombieChecking(self.zombieIntervalID);
+        self.stopZombieMode(self.zombieSpawnIntervalID);
+        self.gameEnded();
+      }
+    });
+  }
+
+  startZombieChecking() {
+    game.zombieIntervalID = window.setInterval(() => {
+      game.checkIfZombieWalkFinished();
+    }, 50);
+  }
+
+  stopZombieChecking(intervalID) {
+    window.clearInterval(intervalID);
+  }
+
+  killZombie() {
+    const zombieSpawner = document.getElementById("zombie-spawner");
+    const zombie = document.querySelector(".zombie");
+    zombie.classList.add("zombie-dead");
+    window.setTimeout(() => {
+      zombieSpawner.removeChild(zombie);
     }, 1000);
   }
-  this.nextQuestion();
-  // Zombie Mode easter egg
-  var robotHead = document.getElementById("robot-head");
-  robotHead.addEventListener("dblclick", function() {
-    self.startZombieMode();
-    drawAttemptString("Welcome to THE PRONUNCIO OF THE DEAD!");
-  });
-};
-
-Game.prototype.timerDecrement = function() {
-  this.timer--;
-  if (this.timer < 0) {
-    this.gameEnded();
-    window.clearInterval(this.timerID);
-    return;
-  }
-};
-
-Game.prototype.handleContinueButton = function() {
-  hideContinueButton();
-  showNextButtons();
-  // disableSpeakButton();
-  if (!this.checkIfVoiceAvailable(this.currentSentence.bcp47)) {
-    disableRevealPronunciationButton();
-  }
-};
-
-Game.prototype.handlePlayAgainButton = function() {
-  buildSplash();
-};
-
-Game.prototype.handleRevealPronunciationButton = function() {
-  this.speakSentence(this.currentSentence.sentence, this.currentSentence.bcp47);
-};
-
-Game.prototype.handleNextButton = function() {
-  this.currentSentenceNum++;
-  this.nextQuestion();
-  hideNextButtons();
-};
-
-Game.prototype.nextQuestion = function() {
-  this.player.collectScore();
-  this.currentSentence = this.sentences[this.currentSentenceNum];
-  if (this.currentSentenceNum < this.gameLength) {
-    fillInData(
-      this.player.name,
-      this.player.score,
-      this.currentSentence.sentence,
-      this.currentSentence.language,
-      this.allowedAttempts
-    );
-    drawFlag(this.currentSentence.bcp47);
-    if (!this.timed) {
-      drawProgressBar(
-        this.getProgressPercent(this.currentSentenceNum, this.gameLength)
-      );
-    }
-    enableSpeakButton();
-    hideContinueButton();
-    hideSpeechBubble();
-    this.player.handleSpeakButtonClick();
-  } else {
-    this.gameEnded();
-  }
-};
-
-Game.prototype.gameEnded = function() {
-  this.addScoreToHighScores();
-  buildGameOver(this.player.score, this.getHighScores(10));
-  if (this.zombieMode) {
-    document.body.classList.remove("bg-secondary");
-  }
-};
-
-Game.prototype.addScoreToHighScores = function() {
-  var scoreObject = { playerName: this.player.name, score: this.player.score };
-  highScores.push(scoreObject);
-};
-
-Game.prototype.cleanString = function(string) {
-  // Strip out punctuation with Regex and trim whitespace.
-  var regex = new RegExp(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+"'\\]/g);
-  return string
-    .replace(regex, "")
-    .trim()
-    .toLowerCase();
-};
-
-Game.prototype.diffStrings = function(string1, string2) {
-  if (!(string1 && string2)) {
-    return null;
-  }
-  var cleanString1 = this.cleanString(string1);
-  var cleanString2 = this.cleanString(string2);
-
-  var diff = JsDiff.diffWordsWithSpace(cleanString1, cleanString2);
-  return diff;
-};
-
-Game.prototype.calculateDiffScore = function(string1, string2) {
-  if (!(string1 && string2)) {
-    return null;
-  }
-
-  var cleanString1 = this.cleanString(string1);
-  var cleanString2 = this.cleanString(string2);
-  var similarity = dice.coefficient(cleanString1, cleanString2);
-  return Math.floor(similarity * 100);
-};
-
-Game.prototype.speakSentence = function(sentence, languageBcp47String) {
-  var utterance = new SpeechSynthesisUtterance(sentence);
-  utterance.lang = languageBcp47String;
-  window.speechSynthesis.speak(utterance);
-};
-
-Game.prototype.getAvailableSpeechSynthesisLanguages = function() {
-  var supported = [];
-  var voices = speechSynthesis.getVoices();
-  voices.forEach(function(voice) {
-    supported.push(voice.lang.slice(0, 2));
-  });
-  return supported;
-};
-
-Game.prototype.checkIfVoiceAvailable = function(languageBcp47String) {
-  var supported = this.getAvailableSpeechSynthesisLanguages();
-  if (supported.indexOf(languageBcp47String) > -1) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-Game.prototype.translateString = function(string, lang) {
-  // Note: This is a temporary method using a disposable free API key. Will eventually be replaced when the app has a backend.
-  var stringURI = encodeURI(string);
-  fetch(
-    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180627T143038Z.1e878579d0dfe5ce.cace0e83481e4da16a038b62e31825c4764bb8c5&text=" +
-      stringURI +
-      "&lang=" +
-      lang +
-      "-en",
-    {
-      method: "get"
-    }
-  )
-    .then(response => response.json())
-    .then(jsonData => drawTranslation(jsonData.text[0]))
-    .catch(err => {
-      //error block
-    });
-};
-
-Game.prototype.getHighScores = function(num) {
-  var scores = highScores.slice(0);
-
-  scores.sort(function(a, b) {
-    return b.score - a.score;
-  });
-
-  return scores.slice(0, num);
-};
-
-Game.prototype.startZombieMode = function() {
-  var self = this;
-  this.zombieMode = true;
-  this.zombieSpawnIntervalID = window.setInterval(function() {
-    self.spawnZombie();
-  }, 4000);
-  game.startZombieChecking();
-  hideInputCard();
-  document.body.classList.add("bg-secondary");
-};
-
-Game.prototype.stopZombieMode = function(zombieSpawnIntervalID) {
-  window.clearInterval(zombieSpawnIntervalID);
-};
-
-Game.prototype.spawnZombie = function() {
-  var zombieSpawner = document.getElementById("zombie-spawner");
-  var zombie = document.createElement("div");
-  zombie.classList.add("zombie");
-  zombie.setAttribute(
-    "style",
-    "left: " + Math.random() * (window.innerWidth - 100) + "px"
-  );
-  zombieSpawner.appendChild(zombie);
-};
-
-Game.prototype.checkIfZombieWalkFinished = function() {
-  var self = this;
-  var zombies = document.querySelectorAll(".zombie:not(.zombie-dead)");
-  var viewportHeight = Math.max(
-    document.documentElement.clientHeight,
-    window.innerHeight
-  );
-  zombies.forEach(function(zombie) {
-    if (zombie.offsetTop > viewportHeight - 80) {
-      self.stopZombieChecking(self.zombieIntervalID);
-      self.stopZombieMode(self.zombieSpawnIntervalID);
-      self.gameEnded();
-    }
-  });
-};
-
-Game.prototype.startZombieChecking = function() {
-  game.zombieIntervalID = window.setInterval(function() {
-    game.checkIfZombieWalkFinished();
-  }, 50);
-};
-
-Game.prototype.stopZombieChecking = function(intervalID) {
-  window.clearInterval(intervalID);
-};
-
-Game.prototype.killZombie = function() {
-  var zombieSpawner = document.getElementById("zombie-spawner");
-  var zombie = document.querySelector(".zombie");
-  zombie.classList.add("zombie-dead");
-  window.setTimeout(function() {
-    zombieSpawner.removeChild(zombie);
-  }, 1000);
-};
+}
--- scripts/player.js
+++ scripts/player.js
@@ -1,102 +1,105 @@
-"use strict";
+class Player {
+  constructor() {
+    this.name = "";
+    this.score = 0;
+    this.currentAttempt = 1;
+    this.currentScore = 0;
+  }
 
-function Player() {
-  this.name = "";
-  this.score = 0;
-  this.currentAttempt = 1;
-  this.currentScore = 0;
+  setName(name) {
+    if (name) {
+      this.name = name;
+    } else {
+      this.name = "Player";
+    }
+  }
+
+  handleSpeakButtonClick() {
+    this.startListening(game.currentSentence.bcp47);
+  }
+
+  startListening(languageBcp47String) {
+    const self = this;
+
+    // Callback on result
+    annyang.addCallback("result", phrases => {
+      self.handleVoiceInput(phrases[0], phrases);
+      self.stopListening();
+    });
+    // Set options
+    annyang.setLanguage(languageBcp47String);
+
+    // Callback on start - Fired as soon as the browser's Speech Recognition engine starts listening
+    annyang.addCallback("start", () => {
+      drawListening();
+    });
+
+    // Callback on end - Fired when the browser's Speech Recognition engine stops
+    annyang.addCallback("end", () => {
+      drawListeningStopped();
+      annyang.removeCallback();
+    });
+
+    // Start listening.
+    annyang.start({ autoRestart: false });
+  }
+
+  stopListening() {
+    annyang.abort();
+  }
+
+  handleVoiceInput(phrase, phrases) {
+    const diff = game.diffStrings(game.currentSentence.sentence, phrase);
+    const score = game.calculateDiffScore(
+      game.currentSentence.sentence,
+      phrase
+    );
+    drawDiffedStrings(diff);
+    drawBadge(score);
+    this.currentScore = score;
+    this.currentAttempt++;
+
+    // Translate attempt, if it is not perfect. If it is, use the predefined translation.
+    if (!(this.currentScore === 100)) {
+      game.translateString(phrase, game.currentSentence.bcp47);
+    } else {
+      drawTranslation(game.currentSentence.translation);
+    }
+
+    if (this.currentScore === 100) {
+      drawAttemptString("Pronuncio understands perfectly!");
+      document.getElementById("success-sound").play();
+      game.handleContinueButton();
+    } else if (this.currentScore > 80) {
+      drawAttemptString("Pronuncio is slightly confused...");
+      document.getElementById("success-sound").play();
+      game.handleContinueButton();
+    } else if (this.currentScore > 40) {
+      drawAttemptString("Pronuncio is very confused by your words, human.");
+      document.getElementById("fail-sound").play();
+      game.handleContinueButton();
+    } else if (this.currentAttempt <= game.allowedAttempts) {
+      drawAttemptString("Pronuncio has no idea what you are talking about.");
+      document.getElementById("fail-sound").play();
+      game.handleContinueButton();
+    } else {
+      drawAttemptString("Pronuncio declares this is hopeless. Let's move on.");
+      document.getElementById("fail-sound").play();
+      game.handleContinueButton();
+    }
+
+    if (game.zombieMode && this.currentScore > 0) {
+      game.killZombie();
+      this.stopListening();
+      window.setTimeout(() => {
+        game.handleNextButton();
+      }, 100);
+    }
+  }
+
+  collectScore() {
+    this.currentAttempt = 1;
+    this.score += this.currentScore;
+    this.currentScore = 0;
+  }
 }
-
-Player.prototype.setName = function(name) {
-  if (name) {
-    this.name = name;
-  } else {
-    this.name = "Player";
-  }
-};
-
-Player.prototype.handleSpeakButtonClick = function() {
-  this.startListening(game.currentSentence.bcp47);
-};
-
-Player.prototype.startListening = function(languageBcp47String) {
-  var self = this;
-
-  // Callback on result
-  annyang.addCallback("result", function(phrases) {
-    self.handleVoiceInput(phrases[0], phrases);
-    self.stopListening();
-  });
-  // Set options
-  annyang.setLanguage(languageBcp47String);
-
-  // Callback on start - Fired as soon as the browser's Speech Recognition engine starts listening
-  annyang.addCallback("start", function() {
-    drawListening();
-  });
-
-  // Callback on end - Fired when the browser's Speech Recognition engine stops
-  annyang.addCallback("end", function() {
-    drawListeningStopped();
-    annyang.removeCallback();
-  });
-
-  // Start listening.
-  annyang.start({ autoRestart: false });
-};
-
-Player.prototype.stopListening = function() {
-  annyang.abort();
-};
-
-Player.prototype.handleVoiceInput = function(phrase, phrases) {
-  var diff = game.diffStrings(game.currentSentence.sentence, phrase);
-  var score = game.calculateDiffScore(game.currentSentence.sentence, phrase);
-  drawDiffedStrings(diff);
-  drawBadge(score);
-  this.currentScore = score;
-  this.currentAttempt++;
-
-  // Translate attempt, if it is not perfect. If it is, use the predefined translation.
-  if (!(this.currentScore === 100)) {
-    game.translateString(phrase, game.currentSentence.bcp47);
-  } else {
-    drawTranslation(game.currentSentence.translation);
-  }
-
-  if (this.currentScore === 100) {
-    drawAttemptString("Pronuncio understands perfectly!");
-    document.getElementById("success-sound").play();
-    game.handleContinueButton();
-  } else if (this.currentScore > 80) {
-    drawAttemptString("Pronuncio is slightly confused...");
-    document.getElementById("success-sound").play();
-    game.handleContinueButton();
-  } else if (this.currentScore > 40) {
-    drawAttemptString("Pronuncio is very confused by your words, human.");
-    document.getElementById("fail-sound").play();
-    game.handleContinueButton();
-  } else if (this.currentAttempt <= game.allowedAttempts) {
-    drawAttemptString("Pronuncio has no idea what you are talking about.");
-    document.getElementById("fail-sound").play();
-    game.handleContinueButton();
-  } else {
-    drawAttemptString("Pronuncio declares this is hopeless. Let's move on.");
-    document.getElementById("fail-sound").play();
-    game.handleContinueButton();
-  }
-
-  if (game.zombieMode && this.currentScore > 0) {
-    game.killZombie();
-    this.stopListening();
-    window.setTimeout(function() {
-      game.handleNextButton();
-    }, 100);
-  }
-};
-
-Player.prototype.collectScore = function() {
-  this.currentAttempt = 1;
-  this.score += this.currentScore;
-  this.currentScore = 0;
-};
